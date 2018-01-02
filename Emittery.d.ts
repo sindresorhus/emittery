653f92116e72a72457b133c7b82e738f31b9149a
Mark Wubben <mark@novemberborn.net>
2018-01-02 17:53:08 +0000

    Update TS code comments
    * Mirror the README
    * Remove unused JSDoc annotations (as far as VS Code and Atom are
    concerned)
    * Alias the unsubscribe function which leads to nicer type hints in VS
    Code and Atom

--- Emittery.d.ts
+++ Emittery.d.ts
@@ -1,84 +1,73 @@
-// Type definitions for emittery
-// Project: emittery
-// Definitions by: Sindre Sorhus <sindresorhus.com>
-
 export = Emittery;
 
-/**
- * Async event emitter.
- */
 declare class Emittery {
-
 		/**
 		 * Subscribe to an event.
 		 *
+		 * Returns an unsubscribe method.
+		 *
 		 * Using the same listener multiple times for the same event will result
 		 * in only one method call per emitted event.
-		 *
-		 * @returns Unsubscribe method.
 		 */
-		on(eventName: string, listener: (eventData?: any) => any): () => void;
+		on(eventName: string, listener: (eventData?: any) => any): Emittery.UnsubscribeFn;
+
+		/**
+		* Remove an event subscription.
+		*
+		* If you don't pass in a `listener`, it will remove all listeners for that
+		* event.
+		*/
+		off(eventName: string, listener?: (eventData?: any) => any): void;
 
 		/**
 		 * Subscribe to an event only once. It will be unsubscribed after the first
 		 * event.
 		 *
-		 * @returns Promise for the event data when eventName is emitted
+		 * Returns a promise for the event data when `eventName` is emitted.
 		 */
 		once(eventName: string): Promise<any>;
-
-		/**
-		 * Unsubscribe to an event.
-		 *
-		 * If you don't pass in a listener, it will remove all listeners for that
-		 * event.
-		 *
-		 * @param [listener]
-		 */
-		off(eventName: string, listener?: (eventData?: any) => any): void;
-
-		/**
-		 * Subscribe to be notified about any event.
-		 *
-		 * @returns A method to unsubscribe
-		 */
-		onAny(listener: (eventName: string, eventData?: any) => any): () => void;
-
-		/**
-		 * Unsubscribe an onAny listener.
-		 *
-		 * If you don't pass in a listener, it will remove all onAny listeners.
-		 *
-		 * @param [listener]
-		 */
-		offAny(listener?: (eventName: string, eventData?: any) => any): void;
 
 		/**
 		 * Trigger an event asynchronously, optionally with some data. Listeners
 		 * are called in the order they were added, but execute concurrently.
 		 *
-		 * Returns a promise for when all the event listeners are done. Done meaning
-		 * executed if synchronous or resolved when an async/promise-returning
-		 * function. You usually wouldn't want to wait for this, but you could for
-		 * example catch possible errors. If any of the listeners throw/reject, the
-		 * returned promise will be rejected with the error, but the other listeners
-		 * will not be affected.
+		 * Returns a promise for when all the event listeners are done. *Done*
+		 * meaning executed if synchronous or resolved when an
+		 * async/promise-returning function. You usually wouldn't want to wait for
+		 * this, but you could for example catch possible errors. If any of the
+		 * listeners throw/reject, the returned promise will be rejected with the
+		 * error, but the other listeners will not be affected.
 		 *
-		 * @returns A promise for when all the event listeners are done.
+		 * Returns a promise for when all the event listeners are done.
 		 */
 		emit(eventName: string, eventData?: any): Promise<void>;
 
 		/**
-		 * Same as `emit`, but it waits for each listener to resolve before
+		 * Same as `emit()`, but it waits for each listener to resolve before
 		 * triggering the next one. This can be useful if your events depend on each
-		 * other. Although ideally they should not. Prefer emit() whenever possible.
+		 * other. Although ideally they should not. Prefer `emit()` whenever
+		 * possible.
 		 *
 		 * If any of the listeners throw/reject, the returned promise will be
-		 * rejected with the error and the remaining listeners will not be called.
+		 * rejected with the error and the remaining listeners will *not* be called.
 		 *
-		 * @returns A promise for the last event listener settle or first one rejecting.
+		 * Returns a promise for when all the event listeners are done.
 		 */
 		emitSerial(eventName: string, eventData?: any): Promise<void>;
+
+		/**
+		 * Subscribe to be notified about any event.
+		 *
+		 * Returns a method to unsubscribe.
+		 */
+		onAny(listener: (eventName: string, eventData?: any) => any): Emittery.UnsubscribeFn;
+
+		/**
+		Remove an `onAny` subscription.
+
+		If you don't pass in a `listener`, it will remove all `onAny` subscriptions.
+		 */
+		offAny(listener?: (eventName: string, eventData?: any) => any): void;
 
 		/**
 		 * Clear all event listeners on the instance.
@@ -86,18 +75,20 @@ declare class Emittery {
 		clear(): void;
 
 		/**
-		 * Count event listeners for the eventName or all events if not specified.
-		 *
-		 * @param eventName
-		 * @returns Listener count.
+		 * The number of listeners for the `eventName` or all events if not
+		 * specified.
 		 */
 		listenerCount(eventName?: string): number;
 }
 
 declare namespace Emittery {
+	/**
+	 * Removes an event subscription.
+	 */
+	type UnsubscribeFn = () => void;
 
 	/**
-	 * A map of event names to the data type they emit.
+	 * Maps event names to their emitted data type.
 	 */
 	interface Events {
 		[eventName: string]: any;
@@ -122,8 +113,8 @@ declare namespace Emittery {
 	 * ```
 	 */
 	class Typed<EventDataMap extends Events, EmptyEvents = never> extends Emittery {
-		on<Name extends keyof EventDataMap>(eventName: Name, listener: (eventData: EventDataMap[Name]) => any): () => void;
-		on<Name extends EmptyEvents>(eventName: Name, listener: () => any): () => void;
+		on<Name extends keyof EventDataMap>(eventName: Name, listener: (eventData: EventDataMap[Name]) => any): Emittery.UnsubscribeFn;
+		on<Name extends EmptyEvents>(eventName: Name, listener: () => any): Emittery.UnsubscribeFn;
 
 		once<Name extends keyof EventDataMap>(eventName: Name): Promise<EventDataMap[Name]>;
 		once<Name extends EmptyEvents>(eventName: Name): Promise<void>;
@@ -131,8 +122,8 @@ declare namespace Emittery {
 		off<Name extends keyof EventDataMap>(eventName: Name, listener?: (eventData: EventDataMap[Name]) => any): void;
 		off<Name extends EmptyEvents>(eventName: Name, listener?: () => any): void;
 
-		onAny<Name extends keyof EventDataMap>(listener: (eventName: Name, eventData: EventDataMap[Name]) => any): () => void;
-		onAny<Name extends EmptyEvents>(listener: (eventName: Name) => any): () => void;
+		onAny<Name extends keyof EventDataMap>(listener: (eventName: Name, eventData: EventDataMap[Name]) => any): Emittery.UnsubscribeFn;
+		onAny<Name extends EmptyEvents>(listener: (eventName: Name) => any): Emittery.UnsubscribeFn;
 
 		offAny<Name extends keyof EventDataMap>(listener?: (eventName: Name, eventData: EventDataMap[Name]) => any): void;
 		offAny<Name extends EmptyEvents>(listener?: (eventName: Name) => any): void;
