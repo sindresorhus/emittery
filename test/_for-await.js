361cddd2bd65336adeee74dac7b3d76d1b9dfa53
Mark Wubben <mark@novemberborn.net>
2018-01-07 14:40:55 +0000

    Fix for-await transpilation
    This syntax is transformed using async-to-generator, not async-generator-functions.

--- test/_for-await.js
+++ test/_for-await.js
@@ -1,9 +1,12 @@
 'use strict';
 const vm = require('vm');
 
+const syntax = require('babel-plugin-syntax-async-generators');
+const transform = require('babel-plugin-transform-async-to-generator');
+
 try {
-	vm.runInNewContext('async () => { for await (const _ of []) {} }');
-	module.exports = require('babel-plugin-syntax-async-generators');
+	new vm.Script('async () => { for await (const _ of []) {} }'); // eslint-disable-line no-new
+	module.exports = {plugins: [syntax]};
 } catch (err) {
-	module.exports = require('babel-plugin-transform-async-generator-functions');
+	module.exports = {plugins: [syntax, transform]};
 }
